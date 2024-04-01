0df27d1a2d1f9170ac5ddf313b08b37843b0ca36
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-04-01 16:44:37 +0600

    Created a new api named /carts/:id, from that a user can get a specific product for delete

--- index.js
+++ index.js
@@ -59,6 +59,12 @@ async function run() {
         const result = await cartsCollection.find(query).toArray();
         res.send(result);
     });
+    app.delete('/carts/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)}
+        const result = await cartsCollection.deleteOne(query);
+        res.send(result)
+    });
 
 
     // Send a ping to confirm a successful connection
